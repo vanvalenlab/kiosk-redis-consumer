694595ce81bd4a3167583f6ff4cf1824811fa93a
William Graf <7930703+willgraf@users.noreply.github.com>
2018-12-12 11:24:54 -0800

    remove tornado

--- redis_consumer/clients.py
+++ redis_consumer/clients.py
@@ -37,8 +37,6 @@ import time
 import numpy as np
 import requests
 import aiohttp
-import tornado
-from tornado import httpclient
 
 
 class Client(object):  # pylint: disable=useless-object-inheritance
