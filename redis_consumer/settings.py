19b892ede94bb90bba1355a7942ee1c9dce64c70
William Graf <7930703+willgraf@users.noreply.github.com>
2019-05-02 19:51:38 -0700

    update default names in settings

--- redis_consumer/settings.py
+++ redis_consumer/settings.py
@@ -51,13 +51,13 @@ REDIS_HOST = config('REDIS_HOST', default='redis-master')
 REDIS_PORT = config('REDIS_PORT', default=6379, cast=int)
 
 # tensorflow-serving client connection
-TF_HOST = config('TF_HOST', default='tf-serving-service')
+TF_HOST = config('TF_HOST', default='tf-serving')
 TF_PORT = config('TF_PORT', default=8500, cast=int)
 TF_TENSOR_NAME = config('TF_TENSOR_NAME', default='image')
 TF_TENSOR_DTYPE = config('TF_TENSOR_DTYPE', default='DT_FLOAT')
 
 # data-processing client connection
-DP_HOST = config('DP_HOST', default='data-processing-service')
+DP_HOST = config('DP_HOST', default='data-processing')
 DP_PORT = config('DP_PORT', default=8080, cast=int)
 
 # gRPC API timeout in seconds (scales with `cuts`)
