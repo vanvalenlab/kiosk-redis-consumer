aeb685491bfadc9550f098e6011cb1ec1a1b4db4
Morgan Schwartz <msschwartz21@gmail.com>
2020-02-23 18:33:17 -0800

    Fix linting error with module imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,14 +14,13 @@
 #
 import os
 import sys
-import shlex
-import mock
 from datetime import datetime
+import mock
+
+import gensidebar
 
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
-
-import gensidebar
 
 # -- Project information -----------------------------------------------------
 
