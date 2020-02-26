4af7b3a6713a7e816bc20868020adada75109cb8
Morgan Schwartz <msschwartz21@gmail.com>
2020-02-25 17:16:52 -0800

    Change location of import statement following path declaration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,11 +16,9 @@ import os
 import sys
 from datetime import datetime
 import mock
-
-import gensidebar
-
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
+import gensidebar
 
 # -- Project information -----------------------------------------------------
 
