efb92c1d21b209bd200d674979f4c3eaf9e120c0
willgraf <7930703+willgraf@users.noreply.github.com>
2020-07-10 12:54:06 -0700

    Update MIBI consumer to inherit from ImageFileConsumer. (#119)
    * Improved testing coverage using fakeredis and pytest-mock (#117)
    
    * install pytest-mock and replace monkeypatching with `mock` library.
    
    * move testing reqs into requirements-test.txt and add 3.8 to test suite.
    
    * update RedisClient tests to use fakeredis.
    
    * directly patch boto3.client
    
    * no need to alias google.cloud.storage in imports.
    
    * Fix storage tests to use a Singleton dummy class instead of global variables.
    
    * proper string fomatting using {} instead of %s
    
    * move DummyStorage and DummyRedis into utils/testing_utils.py to DRY out the tests
    
    * Update tests to use fakeredis client and fewer global variables.
    
    * use save_output for ImageConsumer and MibiConsumer.
    
    pass save_name into the function directly instead of calling HGETALL inside the save_output function.
    
    * inherit from ImageFileConsumer to make better use of the pre/post-process functions.
    
    * add keys for the mibi pre and postprocessing.
    
    * adapt the mibi consumer tests to use the testing_utils.py helpers.
    
    * remove unused imports.
    
    * Fix a few license URLs. (#118)

--- redis_consumer/consumers/mibi_consumer_test.py
+++ redis_consumer/consumers/mibi_consumer_test.py
@@ -7,7 +7,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.github.com/vanvalenlab/kiosk-data-processing/LICENSE
+#     http://www.github.com/vanvalenlab/kiosk-redis-consumer/LICENSE
 #
 # The Work provided may be used for non-commercial academic purposes only.
 # For any other use of the Work, including commercial use, please contact:
@@ -23,157 +23,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Tests for post-processing functions"""
+"""Tests for MibiConsumer"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
-import copy
+import itertools
 
 import numpy as np
-from skimage.external import tifffile as tiff
 
 import pytest
 
 from redis_consumer import consumers
-from redis_consumer import utils
-from redis_consumer import settings
-
-
-def _get_image(frames=2, img_h=256, img_w=256):
-    bias = np.random.rand(img_w, img_h) * 64
-    variance = np.random.rand(img_w, img_h) * (255 - 64)
-    img = np.random.rand(frames, img_w, img_h) * variance + bias
-    return img
-
-
-class Bunch(object):
-    def __init__(self, **kwds):
-        self.__dict__.update(kwds)
-
-
-class DummyRedis(object):
-    # pylint: disable=R0201,W0613
-    def __init__(self, items=None, prefix='predict', status='new'):
-        items = [] if items is None else items
-        self.work_queue = copy.copy(items)
-        self.processing_queue = []
-        self.prefix = '/'.join(x for x in prefix.split('/') if x)
-        self.status = status
-        self._redis_master = self
-        self.keys = [
-            '{}:{}:{}'.format(self.prefix, 'x.tiff', self.status),
-            '{}:{}:{}'.format(self.prefix, 'x.zip', 'other'),
-            '{}:{}:{}'.format('other', 'x.TIFF', self.status),
-            '{}:{}:{}'.format(self.prefix, 'x.ZIP', self.status),
-            '{}:{}:{}'.format(self.prefix, 'x.tiff', 'other'),
-            '{}:{}:{}'.format('other', 'x.zip', self.status),
-        ]
-
-    def rpoplpush(self, src, dst):
-        if src.startswith('processing'):
-            source = self.processing_queue
-            dest = self.work_queue
-        elif src.startswith(self.prefix):
-            source = self.work_queue
-            dest = self.processing_queue
-
-        try:
-            x = source.pop()
-            dest.insert(0, x)
-            return x
-        except IndexError:
-            return None
-
-    def lpush(self, name, *values):
-        self.work_queue = list(reversed(values)) + self.work_queue
-        return len(self.work_queue)
-
-    def lrem(self, name, count, value):
-        self.processing_queue.remove(value)
-        return count
-
-    def llen(self, queue):
-        if queue.startswith('processing'):
-            return len(self.processing_queue)
-        return len(self.work_queue)
-
-    def hmget(self, rhash, *args):
-        return [self.hget(rhash, a) for a in args]
-
-    def hmset(self, rhash, hvals):  # pylint: disable=W0613
-        return hvals
-
-    def expire(self, name, time):  # pylint: disable=W0613
-        return 1
-
-    def hget(self, rhash, field):
-        if field == 'status':
-            return rhash.split(':')[-1]
-        elif field == 'file_name':
-            return rhash.split(':')[1]
-        elif field == 'input_file_name':
-            return rhash.split(':')[1]
-        elif field == 'output_file_name':
-            return rhash.split(':')[1]
-        elif field == 'reason':
-            return 'reason'
-        return False
-
-    def hset(self, rhash, status, value):  # pylint: disable=W0613
-        return {status: value}
-
-    def hgetall(self, rhash):  # pylint: disable=W0613
-        return {
-            'model_name': 'model',
-            'model_version': '0',
-            'postprocess_function': '',
-            'preprocess_function': '',
-            'file_name': rhash.split(':')[1],
-            'input_file_name': rhash.split(':')[1],
-            'output_file_name': rhash.split(':')[1],
-            'status': rhash.split(':')[-1],
-            'children': 'predict:1.tiff:done,predict:2.tiff:failed,predict:3.tiff:new',
-            'children:done': 'predict:4.tiff:done,predict:5.tiff:done',
-            'children:failed': 'predict:6.tiff:failed,predict:7.tiff:failed',
-        }
-
-
-class DummyStorage(object):
-    # pylint: disable=R0201,W0613
-    def __init__(self, num=3):
-        self.num = num
-
-    def download(self, path, dest):
-        if path.lower().endswith('.zip'):
-            paths = []
-            for i in range(self.num):
-                img = _get_image()
-                base, ext = os.path.splitext(path)
-                _path = '{}{}{}'.format(base, i, ext)
-                tiff.imsave(os.path.join(dest, _path), img)
-                paths.append(_path)
-            return utils.zip_files(paths, dest)
-        img = _get_image()
-        tiff.imsave(os.path.join(dest, path), img)
-        return path
-
-    def upload(self, zip_path, subdir=None):
-        return True, True
-
-    def get_public_url(self, zip_path):
-        return True
+from redis_consumer.testing_utils import redis_client, DummyStorage
 
 
 class TestMibiConsumer(object):
     # pylint: disable=R0201
-    def test_is_valid_hash(self):
-        items = ['item%s' % x for x in range(1, 4)]
 
+    def test_is_valid_hash(self, mocker, redis_client):
         storage = DummyStorage()
-        redis_client = DummyRedis(items)
-        redis_client.hget = lambda *x: x[0]
+        mocker.patch.object(redis_client, 'hget', lambda *x: x[0])
 
         consumer = consumers.MibiConsumer(redis_client, storage, 'mibi')
         assert consumer.is_valid_hash(None) is False
@@ -184,30 +54,8 @@ class TestMibiConsumer(object):
         assert consumer.is_valid_hash('mibi:1234567890:file.tiff') is True
         assert consumer.is_valid_hash('mibi:1234567890:file.png') is True
 
-    def test__consume(self):
+    def test__consume(self, mocker, redis_client):
         # pylint: disable=W0613
-        prefix = 'mibi'
-        status = 'new'
-        storage = DummyStorage()
-        redis_client = DummyRedis(prefix, status)
-
-        consumer = consumers.MibiConsumer(redis_client, storage, prefix)
-
-        def _handle_error(err, rhash):
-            raise err
-
-        def grpc_image(data, *args, **kwargs):
-            inner = np.zeros((1, 256, 256, 1))
-            outer = np.zeros((1, 256, 256, 1))
-            fgbg = np.zeros((1, 256, 256, 2))
-            feature = np.zeros((1, 256, 256, 3))
-            return [inner, outer, fgbg, feature]
-
-        def grpc_image_multi(data, *args, **kwargs):
-            return np.array(tuple(list(data.shape) + [2]))
-
-        def grpc_image_list(data, *args, **kwargs):  # pylint: disable=W0613
-            return [data, data]
 
         def make_model_metadata_of_size(model_shape=(-1, 256, 256, 2)):
 
@@ -217,40 +65,63 @@ class TestMibiConsumer(object):
                     'in_tensor_dtype': 'DT_FLOAT',
                     'in_tensor_shape': ','.join(str(s) for s in model_shape),
                 }]
-
             return get_model_metadata
 
-        dummyhash = '{}:new.tiff:{}'.format(prefix, status)
+        def make_grpc_image(model_shape=(-1, 256, 256, 2)):
+            # pylint: disable=E1101
+            shape = model_shape[1:-1]
 
-        model_shape = (-1, 256, 256, 2)
+            def grpc(data, *args, **kwargs):
+                inner = np.random.random((1,) + shape + (1,))
+                outer = np.random.random((1,) + shape + (1,))
+                fgbg = np.random.random((1,) + shape + (2,))
+                feature = np.random.random((1,) + shape + (3,))
+                return [inner, outer, fgbg, feature]
+            return grpc
 
-        consumer._handle_error = _handle_error
-        consumer.grpc_image = grpc_image
+        image_shape = (300, 300, 2)
+        model_shapes = [
+            (-1, 600, 600, 2),  # image too small, pad
+            (-1, 300, 300, 2),  # image is exactly the right size
+            (-1, 150, 150, 2),  # image too big, tile
+        ]
 
-        consumer.get_model_metadata = \
-            make_model_metadata_of_size(model_shape)
+        scales = ['.9', '']
 
-        result = consumer._consume(dummyhash)
-        assert result == consumer.final_status
-        # test with a finished hash
-        result = consumer._consume('{}:test.tiff:{}'.format(
-            prefix, consumer.final_status))
-        assert result == consumer.final_status
-
-        # test with model_name and model_version
-        redis_client.hgetall = lambda x: {
-            'model_name': 'model',
-            'model_version': '0',
-            'postprocess_function': '',
-            'preprocess_function': '',
-            'file_name': 'test_image.tiff',
-            'input_file_name': 'test_image.tiff',
-            'output_file_name': 'test_image.tiff'
+        job_data = {
+            'input_file_name': 'file.tiff',
         }
-        redis_client.hmset = lambda x, y: True
-        consumer = consumers.MibiConsumer(redis_client, storage, prefix)
-        consumer._handle_error = _handle_error
-        consumer.get_model_metadata = make_model_metadata_of_size((-1, 256, 256, 2))
-        consumer.grpc_image = grpc_image
-        result = consumer._consume(dummyhash)
-        assert result == consumer.final_status
+
+        consumer = consumers.MibiConsumer(redis_client, DummyStorage(), 'mibi')
+
+        test_hash = 0
+        # test finished statuses are returned
+        for status in (consumer.failed_status, consumer.final_status):
+            test_hash += 1
+            data = job_data.copy()
+            data['status'] = status
+            redis_client.hmset(test_hash, data)
+            result = consumer._consume(test_hash)
+            assert result == status
+            result = redis_client.hget(test_hash, 'status')
+            assert result == status
+            test_hash += 1
+
+        mocker.patch('redis_consumer.utils.get_image',
+                     lambda x: np.random.random(image_shape))
+
+        for model_shape, scale in itertools.product(model_shapes, scales):
+            metadata = make_model_metadata_of_size(model_shape)
+            grpc_image = make_grpc_image(model_shape)
+            mocker.patch.object(consumer, 'get_model_metadata', metadata)
+            mocker.patch.object(consumer, 'grpc_image', grpc_image)
+
+            data = job_data.copy()
+            data['scale'] = scale
+
+            redis_client.hmset(test_hash, data)
+            result = consumer._consume(test_hash)
+            assert result == consumer.final_status
+            result = redis_client.hget(test_hash, 'status')
+            assert result == consumer.final_status
+            test_hash += 1
