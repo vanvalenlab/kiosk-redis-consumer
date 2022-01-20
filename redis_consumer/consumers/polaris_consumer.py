20f123f60541899d3e3ea61bb8b8084bb35c559c
elaubsch <elaubsch@caltech.edu>
2022-01-19 16:07:52 -0800

    Add tests

--- redis_consumer/consumers/polaris_consumer.py
+++ redis_consumer/consumers/polaris_consumer.py
@@ -23,7 +23,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""MesmerConsumer class for consuming Mesmer whole cell segmentation jobs."""
+"""PolarisConsumer class for consuming Polaris whole cell segmentation jobs."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
