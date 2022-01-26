0c6c7121c8fca732610b2c348e00b653cfe41d59
Emily Laubscher <43769564+elaubsch@users.noreply.github.com>
2022-01-26 15:01:41 -0800

    Update whitespace
    Co-authored-by: Morgan Schwartz <msschwartz21@gmail.com>

--- redis_consumer/consumers/polaris_consumer.py
+++ redis_consumer/consumers/polaris_consumer.py
@@ -137,7 +137,6 @@ class PolarisConsumer(TensorFlowServingConsumer):
             'download_time': timeit.default_timer() - _,
         })
 
-        scale = 1
 
         # detect dimension order and add to redis
         dim_order = self.detect_dimension_order(image, model_name, model_version)
