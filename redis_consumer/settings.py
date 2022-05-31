73b7dbdf009caa958e4e7f31135d15090f123061
elaubsch <elaubsch@caltech.edu>
2022-05-31 16:01:29 -0700

    Update model versions

--- redis_consumer/settings.py
+++ redis_consumer/settings.py
@@ -92,9 +92,9 @@ EXPIRE_TIME = config('EXPIRE_TIME', default=3600, cast=int)
 METADATA_EXPIRE_TIME = config('METADATA_EXPIRE_TIME', default=30, cast=int)
 
 # Tracking settings
-TRACKING_MODEL = config('TRACKING_MODEL', default='TrackingModelInf:4', cast=str)
+TRACKING_MODEL = config('TRACKING_MODEL', default='NuclearTrackingInf:6', cast=str)
 CALIBAN_MODEL = config('CALIBAN_MODEL', default=TRACKING_MODEL, cast=str)
-NEIGHBORHOOD_ENCODER = config('NEIGHBORHOOD_ENCODER', default='TrackingModelNE:2', cast=str)
+NEIGHBORHOOD_ENCODER = config('NEIGHBORHOOD_ENCODER', default='NuclearTrackingNE:6', cast=str)
 
 # tracking.cell_tracker settings TODO: can we extract from model_metadata?
 MAX_DISTANCE = config('MAX_DISTANCE', default=50, cast=int)
@@ -115,7 +115,7 @@ LABEL_DETECT_ENABLED = config('LABEL_DETECT_ENABLED', default=False, cast=bool)
 
 # Mesmer model Settings
 # deprecated model name, use MESMER_MODEL instead.
-MULTIPLEX_MODEL = config('MULTIPLEX_MODEL', default='MultiplexSegmentation:5', cast=str)
+MULTIPLEX_MODEL = config('MULTIPLEX_MODEL', default='MultiplexSegmentation:9', cast=str)
 MESMER_MODEL = config('MESMER_MODEL', default=MULTIPLEX_MODEL, cast=str)
 MESMER_COMPARTMENT = config('MESMER_COMPARTMENT', default='whole-cell')
 
@@ -127,8 +127,8 @@ POLARIS_SCALE = config('POLARIS_SCALE', default=0.38, cast=float)
 
 # Set default models based on label type
 MODEL_CHOICES = {
-    0: config('NUCLEAR_MODEL', default='NuclearSegmentation:5', cast=str),
-    1: config('CYTOPLASM_MODEL', default='CytoplasmSegmentation:4', cast=str),
+    0: config('NUCLEAR_MODEL', default='NuclearSegmentation:6', cast=str),
+    1: config('CYTOPLASM_MODEL', default='CytoplasmSegmentation:5', cast=str),
     2: config('PHASE_MODEL', default='PhaseCytoSegmentation:0', cast=str)
 }
 
